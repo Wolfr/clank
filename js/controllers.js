0caf7db70c1c7a48c773ed849ed6048360e1212f
Johan Ronsse <wolf@wolfslittlestore.be>
2013-05-21 08:57:22 +0200

    Try to default selects using $.scope

--- js/controllers.js
+++ js/controllers.js
@@ -2,13 +2,7 @@ var myApp = angular.module('myApp',[]);
 
 function PhoneCtrl($scope) {
 
-    $scope.filterPhoneChoice = function() {
-        return true;
-    };
-
-    $scope.orientations = [
-        { "value": "h", "text": "Horizontal" },
-        { "value": "v", "text": "Vertical" }
-    ];
+    $scope.phoneChoice = $scope.phoneChoice[0]; // iPhone4
+    $scope.phoneOrientation = $scope.phoneOrientation[0]; // vertical
 
 }
