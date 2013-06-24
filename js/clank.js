d9bd9d662c26eb0e063fdd840090de93a1282e92
Johan Ronsse <wolf@wolfslittlestore.be>
2013-06-24 18:11:36 +0200

    Improve docs and prepare for page transitions/animations

--- js/clank.js
+++ js/clank.js
@@ -1,6 +1,9 @@
 Zepto(function($){
 
   // Fingerblast!
-  var fb = new FingerBlast('.device-body');
+  var fb = new FingerBlast('.cl-toggle, .cl-slider');
+
+  // Add data ignore push to links unrelated to demo
+  // $('#header a, .cl-demo-list a').attr('data-ignore', 'push');
 
 })
