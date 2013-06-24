2fe3f9df952fed3bd670dd8e13a6f7880dc9c53e
Johan Ronsse <wolf@wolfslittlestore.be>
2013-06-24 19:04:32 +0200

    Improve docs and components (photo sliders, bug with table views, start work on fancy radio js)

--- js/clank.js
+++ js/clank.js
@@ -1,9 +1,40 @@
 Zepto(function($){
 
+  /**
+   * Temp code to try and implement anims/page transitions
+   */
+
   // Fingerblast!
-  var fb = new FingerBlast('.cl-toggle, .cl-slider');
+  if ($('.cl-toggle').length > 0) {
+      var fb = new FingerBlast('.cl-toggle');
+  }
+
+  if ($('.cl-slider').length > 0) {
+      var fb = new FingerBlast('.cl-slider');
+  }
 
   // Add data ignore push to links unrelated to demo
   // $('#header a, .cl-demo-list a').attr('data-ignore', 'push');
+  
+  /**
+   * Radio and checkbox lists
+   */
+  
+  $('.radio-list input').change(function() {
+
+    // Remove all checked
+    $(this).parents('.radio-list').find('label').removeClass('checked');
+
+    // Add class so we can style
+    $(this).parent().addClass('checked');
+
+  });
+
+  $('.checkbox-list input').change(function() {
+
+    // Add class so we can style
+    $(this).parent().addClass('checked');
+
+  });
 
 })
