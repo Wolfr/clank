83d0754d1f86a38d28ade98da46dcdf8d8357162
Johan Ronsse <wolf@wolfslittlestore.be>
2013-06-26 15:59:17 +0200

    Make docs visible work with Angular

--- js/docs/controllers.js
+++ js/docs/controllers.js
@@ -1,62 +1,95 @@
 var myApp = angular.module('myApp',['ngCookies']);
 
+/**
+ *  Change interpolation since it clashes with Jekyll
+ */
+
 myApp.config(function($interpolateProvider) {
   $interpolateProvider.startSymbol('{[{');
   $interpolateProvider.endSymbol('}]}');
 });
 
 function DeviceCtrl($scope, $cookieStore) {
+  
+  /**
+   *  Data
+   */
 
-    // Devices data
-    $scope.devices = [
-      { name: 'iPhone 4', os: 'iOS'},
-      { name: 'iPhone 5', os: 'iOS'},
-      { name: 'Galaxy Nexus', os: 'Android'},
-      { name: 'Nexus 7', os: 'Android'},
-      { name: 'iPad 3', os: 'iOS'}
-    ];
+  // Devices data
+  $scope.devices = [
+    { name: 'iPhone 4', os: 'iOS'},
+    { name: 'iPhone 5', os: 'iOS'},
+    { name: 'Galaxy Nexus', os: 'Android'},
+    { name: 'Nexus 7', os: 'Android'},
+    { name: 'iPad 3', os: 'iOS'}
+  ];
 
-    // Orientations data
-    $scope.deviceOrientations = [
-      { name: 'Portrait' },
-      { name: 'Landscape' }
-    ];
-    
-    // When you set a device, also set a cookie
-    $scope.saveDeviceChoice = function() {
-      $cookieStore.put('savedDevice', $scope.deviceChoice.name);
-    }
+  // Orientations data
+  $scope.deviceOrientations = [
+    { name: 'Portrait' },
+    { name: 'Landscape' }
+  ];
+  
+  $scope.deviceSize = 1;
 
-    // When you set an orientation, also set a cookie
-    $scope.saveDeviceOrientation = function() {
-      $cookieStore.put('savedOrientation', $scope.deviceOrientation.name);
-    }
+  /**
+   *  Save cookies when you change settings and apply them by default
+   */
 
-    // If a cookie with saved device data exists apply it
-    if ($cookieStore.get('savedDevice')) {
-      angular.forEach($scope.devices, function (device) {
-        if (device.name == $cookieStore.get('savedDevice')) {
-          $scope.deviceChoice = device;
-        }
-      });
-    } else {
-      // Apply device default
-      $scope.deviceChoice = $scope.devices[0]; 
-    }
+  // Cookie for device choice
+  $scope.saveDeviceChoice = function() {
+    $cookieStore.put('savedDevice', $scope.deviceChoice.name);
+  }
+  
+  if ($cookieStore.get('savedDevice')) {
+    angular.forEach($scope.devices, function (device) {
+      if (device.name == $cookieStore.get('savedDevice')) {
+        $scope.deviceChoice = device;
+      }
+    });
+  } else {
+    $scope.deviceChoice = $scope.devices[0];
+  }
 
-    // If a cookie with saved orientation data exists apply it
-    if ($cookieStore.get('savedOrientation')) {
-      angular.forEach($scope.deviceOrientations, function (deviceOrientation) {
-        if (deviceOrientation.name == $cookieStore.get('savedOrientation')) {
-          $scope.deviceOrientation = deviceOrientation;
-        }
-      });
-    } else {
-      // Apply orientation default
-      $scope.deviceOrientation = $scope.deviceOrientations[0];
-    }
+  // Cookie for device orientation
+  $scope.saveDeviceOrientation = function() {
+    $cookieStore.put('savedOrientation', $scope.deviceOrientation.name);
+  }
+  
+  if ($cookieStore.get('savedOrientation')) {
+    angular.forEach($scope.deviceOrientations, function (deviceOrientation) {
+      if (deviceOrientation.name == $cookieStore.get('savedOrientation')) {
+        $scope.deviceOrientation = deviceOrientation;
+      }
+    });
+  } else {
+    // Apply orientation default
+    $scope.deviceOrientation = $scope.deviceOrientations[0];
+  }
 
-    // Size default
+  // Cookie for docs visibility
+  $scope.saveDocsVisibility = function() {
+    $cookieStore.put('docsVisible', $scope.docsVisible);
+  }
+  
+  // If a cookie with saved docs visibility exists apply it
+  if ($cookieStore.get('docsVisible')) {
+    $scope.docsVisible = true;
+  } else {
+    $scope.docsVisible = false;
+  }
+
+  // Cookie for size
+  $scope.saveDeviceSize = function() {
+    $cookieStore.put('deviceSize', $scope.deviceSize);
+  }
+
+  // If a cookie with device size exists apply it
+  if ($cookieStore.get('deviceSize')) {
+    $scope.deviceSize = $cookieStore.get('deviceSize', $scope.deviceSize);
+  } else {
+    // Size default (this doesn't get saved)
     $scope.deviceSize = 1;
+  }
 
 }
